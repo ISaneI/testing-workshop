2cc1375114021ae30eaa8c40e82ce76e7f6d69d2
ISaneI <mhaidacher97@gmail.com>
2024-01-31 16:10:10 +0100

    hero details test

--- src/app/hero-detail/hero-detail.spec.ts
+++ src/app/hero-detail/hero-detail.spec.ts
@@ -1,13 +1,17 @@
-import { render, screen } from '@testing-library/angular';
+import { render, } from '@testing-library/angular';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from '../hero.service';
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import { MockBuilder, MockInstance } from 'ng-mocks'
+import { of } from 'rxjs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('TimescalesEditorComponent', () => {
-  // TODO: remove once the others are working!!
-  test('should have a test case', async () => {
-    await render(HeroDetailComponent, {
-      providers: [HeroService, HttpClient, HttpHandler],
-    });
-  });
+  MockInstance.scope()
+
+  beforeAll(() => MockInstance(HeroService, 'getHero', () => of({
+    id: 0,
+    name: 'cool Hero'
+  })))
+
+  beforeEach(() => MockBuilder(HeroDetailComponent).mock(FormsModule))
 });
